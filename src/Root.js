6bbf69d7f4c6625bab53102d0993c7fc2ddb9c4d
ahmedabdrabou110 <a.abdrabou1102000@icloud.com>
2023-12-28 00:31:06 +0200

    Add AppBar to My Application

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,35 @@
-import { Typography } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, Link } from "@mui/material";
 
 const Root = () => {
   return (
     <div>
-      <Typography noWrap component="h1" variant="h1">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat eos ut
-        aut, atque quaerat in!
-      </Typography>
-      
+      <AppBar position="static">
+        <Toolbar sx={{ display: "flex", alignItems: "center" }}>
+          <Link
+            href="/"
+            color="inherit"
+            fontSize={20}
+            underline="none"
+            sx={{ flexGrow: 1, "&:hover": { fontSize: "20.75px" } }}
+          >
+            My expenses
+          </Link>
+          <Typography variant="body1" color="white" mr={2}>
+            Ahmed Abdrabou
+          </Typography>
+          <Avatar alt="Ahmed Abdrabou" src="./images/ahmed.jpg" />
+        </Toolbar>
+      </AppBar>
+
       <Outlet />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import react from "react";
 import App from "./App";
+import "./index.css";
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container);
